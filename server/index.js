12b8c2038f04139ce3de0cb66a68b550f5be9725
unknown <farzanafzal24@gmail.com>
2019-08-27 13:11:50 +0500

    hellok

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 // require express
 const express = require('express');
+const path = require("path")
 const app = express();
-
+app.use(express.static(path.resolve(__dirname,"../dist")))
 // run server
 var port = process.env.PORT || 3000;
 app.listen(port, test => {
