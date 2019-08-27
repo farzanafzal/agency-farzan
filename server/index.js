b6610b35c0a57e2b584bdd90c0ecbfd575f65619
unknown <farzanafzal24@gmail.com>
2019-08-27 13:09:03 +0500

    hellok

--- server/index.js
+++ server/index.js
@@ -2,18 +2,6 @@
 const express = require('express');
 const app = express();
 
-// run mongoose
-var mongoose = require('mongoose');
-
-// run middleware
-require('./middleware')(app, express);
-
-// run passport startegy
-// require('./passport');
-
-// run API
-require('./routes')(app);
-
 // run server
 var port = process.env.PORT || 3000;
 app.listen(port, test => {
